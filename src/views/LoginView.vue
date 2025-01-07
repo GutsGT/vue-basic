776731214d7a4a48a019995f159becbe2abd1b52
Otavio ACdeC <gutsgt1@gmail.com>
2025-01-07 11:04:36 -0300

    ./src/views/LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -16,16 +16,18 @@
         <h1 class="text-6xl font-bold text-white mb-16">Login</h1>
 
         <div class="text-black">
-            <form class="flex flex-col items-center" action="" @submit.prevent="login">
-                <div class="w-3/4 mb-5">
+            <form class="*:w-full *:md:w-3/4 flex flex-col items-center" action="" @submit.prevent="login">
+                <div class="mb-5">
                     <input class="w-full rounded-lg p-2" type="email" placeholder="Email" v-model="user.email">
                     <div class="text-white text-left">
                         <template v-if="user.email.length <= 0">Empty Email</template>
                         <template v-else>Email: {{ user.email }}</template>
                     </div>
                 </div>
-                <input class="w-3/4 rounded-lg p-2 mb-5" type="password" placeholder="Password" v-model="user.password">
-                <div class="w-3/4 text-right">
+                <div class="mb-5">
+                    <input class="w-full rounded-lg p-2" type="password" placeholder="Password" v-model="user.password">
+                </div>
+                <div class="text-right">
                     <button class="p-2 rounded-lg bg-white hover:bg-gray-500 transition" type="submit">Login</button>
                 </div>
             </form>
