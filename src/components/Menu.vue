fd4fdd8a2f48bec47b59abd03104bc6fb7ba0463
Otavio ACdeC <gutsgt1@gmail.com>
2025-01-07 11:04:36 -0300

    ./src/components/Menu.vue

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,14 +2,28 @@
 </script>
 
 <template>
-    <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/users">Users</RouterLink>
-    </nav>
+    <div class="text-md md:text-2xl w-full">
+        <nav class="flex text-center mt-10">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/login">Login</RouterLink>
+            <RouterLink to="/users">Users</RouterLink>
+        </nav>
+    </div>
 </template>
 
-<style>
+<style scoped>
+    nav>a {
+        border-left: 2px solid gray;
+        padding: 10px;
+        min-width: 80px;
+    }
 
+    nav>a:last-child{
+        border-right: 2px solid gray;
+    }
+
+    nav a.router-link-exact-active {
+        color: var(--color-primary);
+    }
 </style>
